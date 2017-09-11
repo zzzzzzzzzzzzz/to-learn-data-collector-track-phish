baeb3f8fd5afca27ccbfbabbc6607a12210485e1
Philip Skomorokhov <fscomorohov@rambler.ru>
2017-09-11 07:49:56 +0300

    Add urls logging

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.views.generic import FormView
 from .forms import UrlInputForm
 from django.shortcuts import resolve_url
 from django.http import JsonResponse
+import os.path
 
 
 class IndexView(FormView):
@@ -9,4 +10,9 @@ class IndexView(FormView):
     form_class = UrlInputForm
 
     def form_valid(self, form):
-        return JsonResponse({'urls': str(form.cleaned_data['urls'])})
+        urls = str(form.cleaned_data['urls'])
+        urls_log_file = open(os.path.dirname(__file__) + '/../urls_log.txt', 'a')
+        urls_log_file.write(urls)
+        urls_log_file.write(",\n")
+        urls_log_file.close()
+        return JsonResponse({'urls': urls})
